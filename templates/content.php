59d3c00e3ad6e44745380f19d61f178a6135650c
erikkowalski <erikkowalski@mac.com>
2015-08-13 08:22:03 -0400

    Added headlines on top of featured images, made only home page 760px max width

--- templates/content.php
+++ templates/content.php
@@ -1,11 +1,13 @@
 <article <?php post_class(); ?>>
   <header>
+	  <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+
 	  <?php if ( has_post_thumbnail() ) {the_post_thumbnail();}; ?>
 
-    <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
     <?php get_template_part('templates/entry-meta'); ?>
   </header>
   <div class="entry-summary">
     <?php the_excerpt(); ?>
   </div>
 </article>
+<hr/>
