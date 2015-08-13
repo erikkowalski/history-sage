4a23a83ae58c625bd669b38e8ac537e9830a047f
erikkowalski <erikkowalski@mac.com>
2015-08-13 08:01:24 -0400

    initial commit

---
+++ templates/head.php
@@ -0,0 +1,8 @@
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+	<script src="//use.typekit.net/rtv4geu.js"></script>
+	<script>try{Typekit.load({ async: true });}catch(e){}</script>
+  <?php wp_head(); ?>
+</head>
