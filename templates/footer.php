574511fc379aaca52c73b130dfb725cc4e39e37b
erikkowalski <erikkowalski@mac.com>
2015-08-13 08:04:55 -0400

    removed resources and footer social media links

--- templates/footer.php
+++ templates/footer.php
@@ -3,12 +3,6 @@
     <?php dynamic_sidebar('sidebar-footer'); ?>
 
   </div>
-	<div class="row social-icons container">
 
-		<div class="col-xs-12 col-sm-4  col-md-4"><div class="img-wrap"><a href="http://be.net/erikkowalski"><img src="<?php echo get_template_directory_uri(); ?>/dist/images/behance.png"; alt="behance profile"></a></div></div>
-		<div class="col-xs-12 col-sm-4  col-md-4"><div class="img-wrap"><a href="www.linkedin.com/pub/erik-kowalski/7/883/ba5/"> <img src="<?php echo get_template_directory_uri(); ?>/dist/images/linkedin.png"; alt="linkedin profile"></a></div></div>
-		<div class="col-xs-12 col-sm-4  col-md-4"> <div class="img-wrap"><a href="http://www.pinterest.com/proferik/graphic-design-typography/"> <img src="<?php echo get_template_directory_uri(); ?>/dist/images/pinterest.png" alt="pinterest page"></a></div></div>
-
-	</div>
 	<h5>&copy; Professor Erik Kowalski <?php echo date("Y"); ?></h5>
 </footer>
