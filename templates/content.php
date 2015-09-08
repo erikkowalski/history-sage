ba1cd43fc7cfa8fbc805fa7e3d5648cb2463919a
erikkowalski <erikkowalski@mac.com>
2015-09-08 16:09:49 -0400

    moved type kit to top of head, made featuerd images linkes to posts

--- templates/content.php
+++ templates/content.php
@@ -2,7 +2,7 @@
   <header>
 	  <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
 
-	  <?php if ( has_post_thumbnail() ) {the_post_thumbnail();}; ?>
+	  <a href="<?php the_permalink(); ?>"> <?php if ( has_post_thumbnail() ) {the_post_thumbnail();}; ?></a>
 
     <?php get_template_part('templates/entry-meta'); ?>
   </header>
--- templates/head.php
+++ templates/head.php
@@ -1,8 +1,9 @@
 <head>
+	<script src="//use.typekit.net/rtv4geu.js"></script>
+	<script>try{Typekit.load({ async: true });}catch(e){}</script>
   <meta charset="utf-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-	<script src="//use.typekit.net/rtv4geu.js"></script>
-	<script>try{Typekit.load({ async: true });}catch(e){}</script>
+
   <?php wp_head(); ?>
 </head>
